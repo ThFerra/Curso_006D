6854d08a633d47300eadf342c77f2268880fef15
ThFerra <167789575+ThFerra@users.noreply.github.com>
2024-05-20 11:03:32 -0400

    Saludo.Operaciones

---
+++ 006D/Saludo.py
@@ -0,0 +1,17 @@
+print("Hola como estas")
+print("bienvenido al curso de Python")
+print("4 operaciones basicas")
+print("Ingrese 1° numero:")
+a=int(input("-> "))
+print("Ingrese 2° numero:")
+b=int(input("-> "))
+suma=a+b 
+resta=a-b 
+div=a/b 
+mul=a*b 
+print("Resultados:")
+print("De la suma: ",suma)
+print("De la resta: ",resta)
+print("De la division: ",div)
+print("De la multiplicacion: ",mul)
+print("Adios chao")
